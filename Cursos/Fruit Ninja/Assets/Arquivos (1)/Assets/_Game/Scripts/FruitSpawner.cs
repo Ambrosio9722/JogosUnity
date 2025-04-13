f57ee0e36d315ae6563e3e6f4b8ddedf4ac7102b
Antonio Ambrosio <antonio0900n@gmail.com>
2025-04-13 01:23:04 -0300

    Spawner

---
+++ Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/FruitSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] fruitsPrefab;
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private float minDelay, maxDelay;
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+  private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
+
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            GameObject fruitPrefab = Instantiate(fruitsPrefab[Random.Range(0, fruitsPrefab.Length)], spawnPoint.position, spawnPoint.rotation);
+            Destroy(fruitPrefab, 5f);
+        }
+    }
+}
