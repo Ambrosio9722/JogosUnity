3044f1125d62fee4463cd8a75c3e56b2c2925ecc
Antonio Ambrosio <antonio0900n@gmail.com>
2025-04-16 09:43:25 -0300

    cor frutas

---
+++ Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/GameControler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject splash;
+
+    [HideInInspector] public Color32 apple = new Color32(121,148,12,255), coconut = new Color32(112,71,44, 255), morango = new Color32(167,0,20, 255), banana = new Color32(238,195,27,255), melancia = new Color32(72,108,13, 255);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
