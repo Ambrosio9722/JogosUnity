71834a1fe59a3913be54dbb944dca433a92a1dfb
Antonio Ambrosio <antonio0900n@gmail.com>
2025-04-16 00:28:30 -0300

    aaaa

--- Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/rotate.cs
+++ Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/rotate.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        RotateFruitSliced(Random.Range(350f, 500f));
+    }
+
+    private void RotateFruitSliced(float speed)
+    {
+        this.transform.GetChild(0).transform.Rotate(new Vector3 (0f, speed, speed) * Time.deltaTime);
+        this.transform.GetChild(1).transform.Rotate(new Vector3(0f, -speed, -speed) * Time.deltaTime);
     }
 }
