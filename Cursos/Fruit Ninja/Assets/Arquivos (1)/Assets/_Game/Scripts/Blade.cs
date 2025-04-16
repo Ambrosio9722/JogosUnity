3fef72a5f2f79819ce4b380726411bed808d7373
Antonio Ambrosio <antonio0900n@gmail.com>
2025-04-15 23:39:37 -0300

    mecânica corte 1

--- Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/Blade.cs
+++ Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/Blade.cs
@@ -7,7 +7,7 @@ public class Blade : MonoBehaviour
     private TrailRenderer trailRenderer;
     private CircleCollider2D circleCollider;
     private Vector2 previousPosition;
-    [SerializeField] private float minCuttingVelocity = 0.001f;
+    [SerializeField] private float minCuttingVelocity = 0.0001f;
 
     private void Awake()
     {
@@ -23,6 +23,48 @@ public class Blade : MonoBehaviour
    
     void Update()
     {
-        
+        CutSystem();
     }
+
+private void CutSystem()
+    {
+        if (Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            UpdateCut();
+        }
+        else if (Input.touchCount == 0)
+        {
+            StopCut();
+        }
+    }
+
+    private void UpdateCut()
+    {
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        this.transform.position = newPosition;
+        float velocity = (newPosition - previousPosition).magnitude * Time.deltaTime;
+
+        if (velocity > minCuttingVelocity)
+        {
+            circleCollider.enabled = true;
+            trailRenderer.enabled = true;
+        }
+        else
+        {
+            circleCollider.enabled = false;
+            trailRenderer.enabled = false;
+        }
+        previousPosition = newPosition;
+    }
+    private void StopCut()
+    {
+        if (this.transform.position == null)
+        {
+            this.transform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        }
+        circleCollider.enabled = false;
+        trailRenderer.enabled = false;
+    }
+
+
 }
