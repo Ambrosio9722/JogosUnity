26ab25dbee7c77d4d919df8341424366d4809fe4
Antonio Ambrosio <antonio0900n@gmail.com>
2025-05-23 22:20:16 -0300

    a

---
+++ Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/UIControler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIControler : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
