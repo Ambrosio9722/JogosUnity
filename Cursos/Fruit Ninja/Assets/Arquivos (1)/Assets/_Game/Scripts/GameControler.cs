fd425239cd46613781d11a67b6f2dbde94f3e061
Antonio Ambrosio <antonio0900n@gmail.com>
2025-05-23 23:35:05 -0300

    Interface de usuário

--- Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/GameControler.cs
+++ Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/GameControler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameControler : MonoBehaviour
 {
@@ -8,9 +9,13 @@ public class GameControler : MonoBehaviour
     public GameObject splash;
 
     [HideInInspector] public Color32 appleColor = new Color32(121,148,12,255), coconutColor = new Color32(112,71,44, 255), morangoColor = new Color32(167,0,20, 255), bananaColor = new Color32(238,195,27,255), melanciaColor = new Color32(72,108,13, 255);
+    private UIControler uiController;
+
+    [HideInInspector] public int score;
     void Start()
     {
-        
+        uiController = FindAnyObjectByType<UIControler>();
+        score = 0;
     }
 
     // Update is called once per frame
@@ -18,4 +23,12 @@ public class GameControler : MonoBehaviour
     {
         
     }
+
+    public void StartGame()
+    {
+        uiController.txtHighscore.text = " Score: " + score;
+     }
+
 }
+
+
--- Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/UIControler.cs
+++ Cursos/Fruit Ninja/Assets/Arquivos (1)/Assets/_Game/Scripts/UIControler.cs
@@ -6,15 +6,37 @@ using TMPro;
 
 public class UIControler : MonoBehaviour
 {
-    
+    public TMP_Text tctScore, txtHighscore;
+
+    public Image[] imgLivbes;
+
+    public Button BtnPause, btnResume, btnMainMenu, btnClosePauseMenu, btnSounds;
+
+    public GameObject panelGame, panelPause;
+
+
+
     void Start()
     {
-        
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
     }
 
    
     void Update()
     {
         
+    }
+    public void ButtonPauseGame()
+    {
+        panelGame.gameObject.SetActive(false);
+        panelPause.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ButtonClosePanelPause()
+    {
+        panelGame.gameObject.SetActive(true);
+        panelPause.gameObject.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
