1835cf11edd45b75487908fdee4aa7f41fbb31d7
Antonio Ambrosio <antonio0900n@gmail.com>
2025-08-14 16:53:27 -0300

    Finalização tela de logo

---
+++ Projetos/Carot_2/Assets/SCRIPTS/logo/logo_menu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class logo_menu : MonoBehaviour
+{
+  
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+
+    void ChamarMenu()
+    {
+
+        SceneManager.LoadScene("Menu");
+    }
+}
