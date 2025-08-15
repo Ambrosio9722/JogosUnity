ef972c16dfe957c5b10b78e23cbb872cc2edf90e
Antonio Ambrosio <antonio0900n@gmail.com>
2025-08-15 19:14:33 -0300

    andar e virar player

--- Projetos/Carot_2/Assets/SCRIPTS/Player/player.cs
+++ Projetos/Carot_2/Assets/SCRIPTS/Player/player.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    
+    private Rigidbody2D corpo;
+    public float velocidade;
+    private SpriteRenderer sprit;
+
     void Start()
     {
-        
+        corpo = GetComponent<Rigidbody2D>(); // pegar um componente na unity
+        sprit = GetComponent<SpriteRenderer>(); // virar o player 
     }
 
  
     void Update()
     {
-        
+        //andar
+        float horizontal = Input.GetAxis("Horizontal");
+        corpo.linearVelocity = new Vector2(horizontal*velocidade, corpo.linearVelocity.y);
+
+        // virar sprit
+        flip(horizontal);
+    }
+
+    private void flip(float horizontal)
+    {
+        if (horizontal > 0)
+        {
+            sprit.flipX = false;
+        }
+        else if (horizontal < 0)
+        {
+            sprit.flipX = true;
+        }
+
     }
 }
