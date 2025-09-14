a51db502098dde635df9fb1e75fd4414b7525761
Antonio Ambrosio <antonio0900n@gmail.com>
2025-09-14 03:00:16 -0300

    primeira build

--- Projetos/Carot_2/Assets/SCRIPTS/Inimigos/inimigo.cs
+++ Projetos/Carot_2/Assets/SCRIPTS/Inimigos/inimigo.cs
@@ -1,5 +1,5 @@
 using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
+
 using UnityEngine;
 
 public class inimigo : MonoBehaviour
@@ -10,42 +10,17 @@ public class inimigo : MonoBehaviour
     
     void Start()
     {
-      //  rb = GetComponent<Rigidbody2D>();
+      
     }
 
    
     void Update()
     {
-        //  Mover();
+      
         transform.Translate(Vector2.left* velocidade*Time.deltaTime);
     }
 
-   // private void Mover()
-   // {
-  //      rb.linearVelocity = new Vector2(velocidade, rb.linearVelocity.y);
-  //  }
-
- //   private void OnTriggerExit2D(Collider2D collision)
- //   {
- //       velocidade *= -1;
- //       Flip();
- //   }
-
- //   private void Flip()
- //   {
- //       Vector3 currentScale = gameObject.transform.localScale;
- //       currentScale.x *= -1;
- //       gameObject.transform.localScale = currentScale;
- //   }
-
- // void OnTriggerEnter2D(Collider2D col)
- //   {
- //       if (col.gameObject.CompareTag("bala")) // bala
- //       {
- //           Destroy(col.gameObject); // destroi bala
-//            Destroy(this.gameObject);
-//        }
-//    }
+ 
 
     private void FlipEnemy()
     {
