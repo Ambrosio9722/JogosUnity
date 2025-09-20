d5e9d2c999188d5cf7e30221edf0071c11ffb6cb
Antonio Ambrosio <antonio0900n@gmail.com>
2025-09-20 00:46:25 -0300

    mecânica teleporte 2.0

--- Projetos/Carot_2/Assets/SCRIPTS/Player/playerTeleport.cs
+++ Projetos/Carot_2/Assets/SCRIPTS/Player/playerTeleport.cs
@@ -2,51 +2,30 @@ using UnityEngine;
 
 public class playerTeleport : MonoBehaviour
 {
-    //  private GameObject currentTeleport;
-    //  public bool podeTP;
-    //  float tempo = 0;
+  
     public GameObject portal;
     private GameObject player;
+    public int DireitaEsquerda;  // -1 1
     void Start()
     {
-        //      podeTP = true;
+        
         player = GameObject.FindWithTag("player");
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-  //      if (currentTeleport != null)
-  //      {
-  //          transform.position = currentTeleport.GetComponent<Teleport>().GetDestination().position;
-  //      }
+ 
     }
 
   private void OnTriggerEnter2D(Collider2D col)
    {
-        //      if (col.CompareTag("teleporte") && podeTP == true) 
-        //      {
-        //           currentTeleport = col.gameObject;
-        //          podeTP = false;
-        //          tempoTP();
-        //      }
+       
 
         if (col.tag == "player")
         {
-            player.transform.position = new Vector2(portal.transform.position.x+1,portal.transform.position.y);
+            player.transform.position = new Vector2(portal.transform.position.x+DireitaEsquerda,portal.transform.position.y);
         }
    }
 
-  //  private void tempoTP()
-  //  {
-  //      tempo += Time.deltaTime;
-
-  //      if (tempo >= 2f)
-  //      {
-
-  //          podeTP = true;
-  //              tempo = 0;
-  //       
-  //      }
- //   }
 }
