af7426a569b2c52f94eb3be4a9941ff32f53c70b
Antonio Ambrosio <antonio0900n@gmail.com>
2025-09-20 00:40:02 -0300

    finalização teleporte

--- Projetos/Carot_2/Assets/SCRIPTS/Player/playerTeleport.cs
+++ Projetos/Carot_2/Assets/SCRIPTS/Player/playerTeleport.cs
@@ -2,26 +2,51 @@ using UnityEngine;
 
 public class playerTeleport : MonoBehaviour
 {
-    private GameObject currentTeleport;
+    //  private GameObject currentTeleport;
+    //  public bool podeTP;
+    //  float tempo = 0;
+    public GameObject portal;
+    private GameObject player;
     void Start()
     {
-        
+        //      podeTP = true;
+        player = GameObject.FindWithTag("player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentTeleport != null)
-        {
-            transform.position = currentTeleport.GetComponent<Teleport>().GetDestination().position;
-        }
+  //      if (currentTeleport != null)
+  //      {
+  //          transform.position = currentTeleport.GetComponent<Teleport>().GetDestination().position;
+  //      }
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("teleporte"))
+  private void OnTriggerEnter2D(Collider2D col)
+   {
+        //      if (col.CompareTag("teleporte") && podeTP == true) 
+        //      {
+        //           currentTeleport = col.gameObject;
+        //          podeTP = false;
+        //          tempoTP();
+        //      }
+
+        if (col.tag == "player")
         {
-            currentTeleport = col.gameObject;
+            player.transform.position = new Vector2(portal.transform.position.x+1,portal.transform.position.y);
         }
-    }
+   }
+
+  //  private void tempoTP()
+  //  {
+  //      tempo += Time.deltaTime;
+
+  //      if (tempo >= 2f)
+  //      {
+
+  //          podeTP = true;
+  //              tempo = 0;
+  //       
+  //      }
+ //   }
 }
